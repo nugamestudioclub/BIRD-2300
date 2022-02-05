075c6e7491e08b91e5a8a01989d368efedc804c5
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-04 22:22:58 -0500

    Merged World with GameManager and fixed more stupid errors

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,49 +1,4 @@
 using UnityEngine;
 
 public class World : MonoBehaviour {
-	[SerializeField]
-	private Vetor3 windowedPosition;
-	
-	[SerializeField]
-	private Vector3 windowedEulerAngles;
-	
-	[SerializeField]
-	private Vector3 fullscreenPosition;
-	
-	[SerializeField]
-	private Vector3 fullscreenEulerAngles;]
-	
-	[SerializeField]
-	private Camera cam;
-	
-	private bool fullscreen = true;
-	
-	public SwitchCamera() {
-		if( fullScreen )
-			SetWindowed();
-		else
-			SetFullscreen();
-	}
-	
-	public SetFullScreen() {
-		cam.transform.position = fullscreenPosition;
-		
-		cam.transform.Rotate(
-			windowedEulerAngles.x - cam.transform.rotation.eulerAngles.x,
-			windowedEulerAngles.y - cam.transform.rotation.eulerAngles.y,
-			windowedEulerAngles.z - cam.transform.rotation.eulerAngles.z
-		);
-		fullscreen = true;
-	}
-	
-	public SetWindowed() {
-		cam.transform.position = windowedPosition;
-		
-		cam.transform.Rotate(
-			fullscreenEulerAngles.x - cam.transform.rotation.eulerAngles.x,
-			fullscreenEulerAngles.y - cam.transform.rotation.eulerAngles.y,
-			fullscreenEulerAngles.z - cam.transform.rotation.eulerAngles.z
-		);
-		fullScreen = false;
-	}
 }
