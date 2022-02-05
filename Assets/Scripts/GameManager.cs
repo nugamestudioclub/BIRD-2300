c889d40a0f5f0ceeb831ae2438f08032906d938e
Sebastian Kobori <sjkobori@gmail.com>
2022-02-04 22:12:24 -0500

    Fixed file structure issues

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,46 @@ public class GameManager : MonoBehaviour
         isTabbbedOut = false;
     }
 
-    void Awake()
+	[SerializeField]
+	private Vector3 windowedPosition;
+
+	[SerializeField]
+	private Vector3 windowedEulerAngles;
+
+	[SerializeField]
+	private Vector3 fullscreenPosition;
+
+	[SerializeField]
+	private Vector3 fullscreenEulerAngles;
+
+	[SerializeField]
+	private Camera cam;
+
+	private bool fullscreen = true;
+
+	public void SwitchCamera()
+	{
+		if (fullscreen)
+			SetWindowed();
+		else
+			SetFullscreen();
+	}
+
+	public void SetFullscreen()
+	{
+		cam.transform.position = fullscreenPosition;
+		//cam.transform.rotation = windowedEulerAngles;
+		fullscreen = true;
+	}
+
+	public void SetWindowed()
+    {
+        cam.transform.position = windowedPosition;
+       // cam.transform.rotation = windowedEulerAngles;
+        fullscreen = false;
+    }
+
+	void Awake()
     {
         Instance = this;
     }
