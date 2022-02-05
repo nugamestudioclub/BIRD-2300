6eec7574c565e87fcc6579da2227c23231a7c21a
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-04 22:59:51 -0500

    Added Player class with scores for letter grade and birdiness

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+	class Player : MonoBehaviour {
+		private int grade;
+
+		public string LetterGrade => grade switch {
+			> 100 => "S",
+			(>= 97) and (<= 100) => "A+",
+			(>= 94) and (< 97) => "A",
+			(>= 90) and (< 94) => "A-",
+			(>= 87) and (< 90) => "B+",
+			(>= 84) and (< 87) => "B",
+			(>= 80) and (< 83) => "B-",
+			(>= 77) and (< 80) => "C+",
+			(>= 74) and (< 77) => "C",
+			(>= 70) and (< 73) => "C-",
+			(>= 67) and (< 70) => "D+",
+			(>= 64) and (< 67) => "D",
+			_ => "F"
+		};
+
+		public int Grade { get; set; }
+
+		public int Birdiness { get; set; }
+	}
+}
