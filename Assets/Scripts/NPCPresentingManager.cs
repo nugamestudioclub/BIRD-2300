a9bbea1c3d8f9049920c55d1570610c75d8dbea5
firez2469 <hampel.m@northeastern.edu>
2022-02-05 12:01:11 -0500

    Added Presenter + Presentor animation controller

---
+++ Assets/Scripts/NPCPresentingManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPresentingManager : MonoBehaviour
+{
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitForNextAnim());
+    }
+
+   IEnumerator waitForNextAnim()
+    {
+        yield return new WaitForSeconds(4);
+        int state = Random.Range(0,3);
+        switch (state) {
+            case 0:
+                anim.SetBool("Idle", true);
+                anim.SetBool("Pointing_Left", false);
+                anim.SetBool("Pointing_Right", false);
+                break;
+            case 1:
+                anim.SetBool("Idle",false);
+                anim.SetBool("Pointing_Left", true);
+                anim.SetBool("Pointing_Right", false);
+                break;
+            case 2:
+                anim.SetBool("Idle", false);
+                anim.SetBool("Pointing_Left", false);
+                anim.SetBool("Pointing_Right", true);
+                break;
+        }
+        StartCoroutine(waitForNextAnim());
+
+    }
+}
