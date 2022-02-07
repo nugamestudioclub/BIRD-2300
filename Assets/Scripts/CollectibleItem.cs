1c10a9a8b9355e688eaff01c41bab58607c64ad9
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-06 21:40:55 -0500

    added bird fact model

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -6,6 +6,13 @@ public class CollectibleItem : MonoBehaviour {
 	[SerializeField]
 	private bool destroyOnCollect = true;
 
+	[SerializeField]
+	private bool spinning = true;
+
+	[SerializeField]
+	[Range(0.0f, float.MaxValue)]
+	private float spinSpeed = 100.0f;
+
 	void OnTriggerEnter(Collider collision) {
 		if( collision.tag == "Player" )
 			Collect();
@@ -15,5 +22,10 @@ public class CollectibleItem : MonoBehaviour {
 		GameManager.Instance.Notebook?.createNewEntry();
 		if( destroyOnCollect )
 			Destroy(gameObject);
+	}
+
+	void Update() {
+		if( spinning )
+			transform.Rotate(0.0f, spinSpeed * Time.deltaTime, 0.0f);
 	}
 }
