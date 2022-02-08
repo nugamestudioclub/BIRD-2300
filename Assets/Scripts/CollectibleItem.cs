25ee7511ce7480963c5cad9df500f1abc8a1ea3d
Sebastian Kobori <sjkobori@gmail.com>
2022-02-07 21:46:38 -0500

    Added collectable sound

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -6,6 +6,9 @@ public class CollectibleItem : MonoBehaviour {
 	[SerializeField]
 	private bool destroyOnCollect = true;
 
+	private bool isCollected = false;
+	private float collectedTime;
+
 	[SerializeField]
 	private bool spinning = true;
 
@@ -13,19 +16,35 @@ public class CollectibleItem : MonoBehaviour {
 	[Range(0.0f, float.MaxValue)]
 	private float spinSpeed = 100.0f;
 
+	[SerializeField]
+	AudioSource audioSource;
+
+	[SerializeField]
+	AudioClip collectSound;
+
 	void OnTriggerEnter(Collider collision) {
-		if( collision.tag == "Player" )
+		if( collision.tag == "Player" ) 
 			Collect();
 	}
 
 	private void Collect() {
 		GameManager.Instance.Notebook?.createNewEntry();
-		if( destroyOnCollect )
-			Destroy(gameObject);
+		audioSource.PlayOneShot(collectSound);
+		if (destroyOnCollect)
+			isCollected = true;
 	}
 
 	void Update() {
 		if( spinning )
 			transform.Rotate(0.0f, spinSpeed * Time.deltaTime, 0.0f);
+		if (isCollected)
+        {
+			collectedTime += Time.deltaTime;
+			if (collectedTime > collectSound.length)
+			{
+				Destroy(gameObject);
+			}
+		}
+		
 	}
 }
